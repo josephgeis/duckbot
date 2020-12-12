df1714817c4817b2ee1e43dbb02115dda34e633d
TheLDB <ldb@erikboles.com>
2020-12-11 21:27:32 -0700

    Did stuff

--- main.py
+++ main.py
@@ -1,19 +1,27 @@
 from discord import Client
 import re
-rule = ["shit", "piss", "fuck", "fucking"]
 token = ""
 # I got rid of the config file to consolidate it to one file cuz simplicity
 # The old bot was picking up words like "briefcase" as profanity because of the regex, just switched it to a list and its checking if it contains.
 # Same process, different method. 
+
 class DuckbotBot(Client):
     async def on_ready(self):
         print(f"Connected to Discord as {self.user}.")
 
+
     async def on_message(self, message):
-         if message.content.lower() in rule:
-                await message.add_reaction(u"\U0001f986")
-                await message.channel.send("Quack! Watch your language.")
-                print("\U0001f986", end="")
+        with open('words.txt') as words:
+            words = words.read().split(',')
+
+        lmc = message.content.lower()
+        
+        for words in filter(lmc.__contains__, words):
+            await message.add_reaction(u"\U0001f986")
+            await message.channel.send("Quack! Watch your language.")
+            raise StopIteration
+            print("\U0001f986", end="")
+
 
 
 bot = DuckbotBot()
