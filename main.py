677179482fc5da669f70ced7814261ed67ed45c2
Z V <edazpotato@gmail.com>
2020-12-15 17:06:19 +1300

    bye-bye hard tabs :(

--- main.py
+++ main.py
@@ -7,29 +7,29 @@ rule = re.compile(config.RULE)
 
 
 class DuckbotBot(Client):
-	async def on_ready(self):
-		print(f"Connected to Discord as {self.user}.")
+    async def on_ready(self):
+        print(f"Connected to Discord as {self.user}.")
 
-	async def on_message(self, message):
-		dm = isinstance(message.channel, discord.DMChannel)
-		if not dm and message.guild.unavailable:
-			return
-		if rule.search(message.content):
-			# Are we allowed to react?
-			# For some reason adding reactions randomly fails, so i'm wrapping it in a try/catch block
-			try:
-				await message.add_reaction(u"\U0001f986")
-			except Exception:
-				pass
-			
-			# Are we allowed to send messages?
-			if dm or message.channel.permissions_for(message.guild.me).send_messages:
-				await message.channel.send("Quack! Watch your language.")
-			print("\U0001f986", end="")
+    async def on_message(self, message):
+        dm = isinstance(message.channel, discord.DMChannel)
+        if not dm and message.guild.unavailable:
+            return
+        if rule.search(message.content):
+            # Are we allowed to react?
+            # For some reason adding reactions randomly fails, so i'm wrapping it in a try/catch block
+            try:
+                await message.add_reaction(u"\U0001f986")
+            except Exception:
+                pass
+            
+            # Are we allowed to send messages?
+            if dm or message.channel.permissions_for(message.guild.me).send_messages:
+                await message.channel.send("Quack! Watch your language.")
+            print("\U0001f986", end="")
 
 
 bot = DuckbotBot()
 
 
 if __name__ == '__main__':
-	bot.run(config.TOKEN)
+    bot.run(config.TOKEN)
